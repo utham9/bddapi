2656c5dc7be770c82340f6a167861d5066320573
Utham Kumar <uthamkmr@gmail.com>
2021-08-15 14:56:37 +0530

    Cache Manager added

---
+++ src/main/java/com/example/qa/api/data/CacheManager.java
@@ -0,0 +1,46 @@
+package com.example.qa.api.data;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CacheManager {
+
+  private static CacheManager instance;
+  private static final Object monitor = new Object();
+  private final Map<String, Object> cache =
+      Collections.synchronizedMap(new HashMap<String, Object>());
+
+  private CacheManager() {}
+
+  public void put(String cacheKey, Object value) {
+    cache.put(cacheKey, value);
+  }
+
+  public Object get(String cacheKey) {
+    return cache.get(cacheKey);
+  }
+
+  public boolean contains(String cacheKey) {
+    return cache.containsKey(cacheKey);
+  }
+
+  public void clear(String cacheKey) {
+    cache.put(cacheKey, null);
+  }
+
+  public void clear() {
+    cache.clear();
+  }
+
+  public static CacheManager getInstance() {
+    if (instance == null) {
+      synchronized (monitor) {
+        if (instance == null) {
+          instance = new CacheManager();
+        }
+      }
+    }
+    return instance;
+  }
+}
